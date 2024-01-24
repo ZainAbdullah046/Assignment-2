82553d6ef7dce9b1ed6f2b111db60ea45af44613
Zain <zainabdullah@gmail.com>
2024-01-24 13:22:50 +0500

    Add function of list

---
+++ Ques2.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<String> days = [];
+  days.add("Monday");
+  days.add("Tuesday");
+  days.add("Wednesday");
+  days.add("Thursday");
+  days.add("Friday");
+  days.add("Saturday");
+  days.add("Sunday");
+
+  //for all to add in one statement
+//  days.addAll(["monday","tuesday", "wednesday", "thursday", "friday",
+//  "satureday", "sunday"]);
+
+  print(days);
+}
