734100d7eac0fd83037a83c4d1042387efbc4aa3
Zain <zainabdullah@gmail.com>
2024-01-24 14:19:21 +0500

    smallest and largest number

---
+++ Ques4.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<int> numbers = [2, 4, 8, 1, 4, 7, 56, 23, 5];
+
+  int smallest =
+      numbers.reduce((value, element) => value < element ? value : element);
+  int largest =
+      numbers.reduce((value, element) => value > element ? value : element);
+  print(" Smallest number of the list is : ${smallest}");
+  print(" Largest number  of the list is : ${largest}");
+
+  print(numbers);
+}
