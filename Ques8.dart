e64e523b451ebfb00344c5d5c3188c9e4b36b0dc
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-01-26 02:31:30 +0500

    remove the false one from the List

--- Ques8.dart
+++ Ques8.dart
@@ -9,10 +9,3 @@ void main() {
   usersEligibility.retainWhere((user) => user["eligible"] == true);
   print(usersEligibility);
 }
-/*List<Map<String, bool>> usersEligibility = [
-{'name': 'John', 'eligible': true},
-{'name': 'Alice', 'eligible': false},
-{'name': 'Mike', 'eligible': true},
-{'name': 'Sarah', 'eligible': true},
-{'name': 'Tom', 'eligible': false},
-];*/
