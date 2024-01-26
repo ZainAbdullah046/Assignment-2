78b6b96cbce3944fda50a4a193d2986ead927e40
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-01-26 23:53:10 +0500

    Reverse the lisst by not changing the original list

---
+++ Ques12.dart
@@ -0,0 +1,17 @@
+//: Write a Dart code that takes in a list of strings and prints a new list with
+//the elements in reverse order. The original list should remain unchanged
+void main() {
+  List<String> OriginalList = [
+    "Zain",
+    "Adil",
+    "Zoha",
+    "Xara",
+    "Abiha",
+    "Suha",
+    "Sufyan"
+  ];
+
+  List<String> ReverseList = List.from(OriginalList.reversed);
+  print(OriginalList);
+  print(ReverseList);
+}
